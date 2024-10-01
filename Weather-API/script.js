279abefbb95749a34b6595c88e071f31b4a8014f
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-01 14:52:28 +0530

    404 error api

--- Weather-API/script.js
+++ Weather-API/script.js
@@ -1,18 +1,24 @@
 const apikey="ba6dc4a5556d3ce4966e6c6b109305db";
 const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-
-
-
-
 const input=document.querySelector('.input');
 async function search(city){
+   
     const response=await fetch(apiurl +city+ `&appid=${apikey}`);
     const data=await response.json();
+  
     console.log(data);
 
+    if(data.cod==='404'){
+        document.querySelector('.city').innerHTML='City Not Found';
+        document.querySelector('.temp').innerHTML="";
+
+    
+    }
+    else{
     document.querySelector('.city').innerHTML=data.name;
     document.querySelector('.temp').innerHTML=data.main.temp + "C";
+    }
 
 }
 function clicked(){
