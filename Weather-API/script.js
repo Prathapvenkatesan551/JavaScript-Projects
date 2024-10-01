9d1b9a804c33b7bafa5a03af24534c5e642af074
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-01 14:40:38 +0530

    Weather Api

---
+++ Weather-API/script.js
@@ -0,0 +1,22 @@
+const apikey="ba6dc4a5556d3ce4966e6c6b109305db";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+
+
+
+const input=document.querySelector('.input');
+async function search(city){
+    const response=await fetch(apiurl +city+ `&appid=${apikey}`);
+    const data=await response.json();
+    console.log(data);
+
+    document.querySelector('.city').innerHTML=data.name;
+    document.querySelector('.temp').innerHTML=data.main.temp + "C";
+
+}
+function clicked(){
+search(input.value);
+}
+
+search(input.value);
