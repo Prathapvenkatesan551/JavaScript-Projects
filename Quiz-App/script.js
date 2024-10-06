a7b8f10b6db63a700cd762d042de5cddb8f6c4a4
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-06 12:33:24 +0530

    quiz app

---
+++ Quiz-App/script.js
@@ -0,0 +1,107 @@
+const questions=[
+    {
+        question:"Which is even number ?",
+        answer:[
+            { text : 2,correct : true},
+            { text : 1,correct : false},
+            { text : 5,correct : false},
+            { text : 7,correct : false},
+            
+        ]
+    },
+    
+    {
+        question:"Which is even number ?",
+        answer:[
+            { text : 2,correct : true},
+            { text : 1,correct : false},
+            { text : 5,correct : false},
+            { text : 7,correct : false},
+            
+        ]
+    }
+]
+
+const questionEle=document.querySelector('.question');
+const anssectionEle=document.querySelector('.ans-section');
+const nextEle=document.querySelector('.next');
+
+// const app=document.querySelector('.content');
+let currentIndex=0;
+let score=0;
+
+function startQuiz(){
+    currentIndex=0;
+    score=0;
+    nextEle.innerHTML="Next";
+   
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion=questions[currentIndex];
+    let qn=currentIndex+1;
+    questionEle.innerHTML= qn +" . "+currentQuestion.question;
+
+    currentQuestion.answer.forEach(answer =>{
+        const button=document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        anssectionEle.appendChild(button);
+        if(answer.correct)
+        {
+            button.dataset.correct=answer.correct;
+        }
+        button.addEventListener("click",checkanswer);
+    });
+
+    
+}
+
+function resetState(){
+    nextEle.style.display ="none";
+    while(anssectionEle.firstChild){
+        anssectionEle.removeChild(anssectionEle.firstChild);
+
+    }
+}
+
+function checkanswer(e,index){
+    const selectedbtn=e.target;
+   const iscorrect=selectedbtn.dataset.correct === "true";
+    if(iscorrect){  
+        score+=1;
+        selectedbtn.classList.add("correct");
+       
+        // alert("correct answer");
+    }
+    else{
+        selectedbtn.classList.add("Incorrect");
+    }
+    Array.from(anssectionEle.children).forEach(butt=>{
+        butt.disabled =true;
+    })
+    nextEle.style.display="block";
+    
+}
+
+nextEle.addEventListener("click",function nextFun(){
+    if(currentIndex<questions.length){
+
+        currentIndex+=1;
+        if(currentIndex<questions.length){
+            showQuestion();
+        }
+        else{
+            const cont=document.querySelector('.content');
+            alert("you completed press ok to restart");
+            startQuiz();
+        }
+    }
+    else{
+        startQuiz();
+    }
+   
+}); 
+startQuiz();
