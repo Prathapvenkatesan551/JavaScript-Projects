7f8c82a7836d4bfc95b252f5cdbfd5555d6dea25
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-11-02 20:41:27 +0530

    Delete Attribute

--- Notes/script.js
+++ Notes/script.js
@@ -1,15 +1,25 @@
 const noteCont = document.querySelector('.note-cont');
 const create = document.querySelector('.new');
-
+localStorage.clear();
 create.addEventListener("click", () => {
     let box = document.createElement("p");
     box.className = "inp";
     box.setAttribute("contenteditable", "true");
     noteCont.appendChild(box);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+    deleteButton.innerText = "Delete"; 
+    box.appendChild(deleteButton);
     update();
 });
 
 noteCont.addEventListener("click", function(e) {
+    if (e.target.classList.contains("delete")) {
+      
+        e.target.parentElement.remove(); 
+        update(); 
+    }
     if (e.target.tagName === "P") {
         const notes = document.querySelectorAll('.inp');
         notes.forEach(element => {
